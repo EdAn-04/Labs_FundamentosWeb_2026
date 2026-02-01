139c8252d3104c0c2ee08a4ba5658f5eca6dee25
EdAn-04 <andres.culajay@galileo.edu>
2026-02-01 14:06:14 -0600

    avances

--- Lab01_RPGDice/app/src/main/java/com/example/lab01_rpgdice/MainActivity.kt
+++ Lab01_RPGDice/app/src/main/java/com/example/lab01_rpgdice/MainActivity.kt
@@ -3,13 +3,11 @@ package com.example.lab01_rpgdice
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.compose.runtime.*
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.lab01_rpgdice.ui.theme.Lab01_RPGDiceTheme
 
@@ -29,13 +27,18 @@ fun RPGDiceScreen() {
     var vitality by remember { mutableIntStateOf(10) }
     var dexterity by remember { mutableIntStateOf(10) }
     var wisdom by remember { mutableIntStateOf(10) }
+
+    val total = vitality + dexterity + wisdom
+
     Column {
         Text("RPG Dice Roller")
 
-        val total = vitality + dexterity + wisdom
+        StatRow("Vitality", vitality) { vitality = (1..25).random() }
+        StatRow("Dexterity", dexterity) { dexterity = (1..25).random() }
+        StatRow("Wisdom", wisdom) { wisdom = (1..25).random() }
 
         Text("Total: $total")
-        // Validaciones
+
         if (total < 30) {
             Text("Re-roll recommended!", color = Color.Red)
         } else if (total >= 50) {
@@ -44,7 +47,6 @@ fun RPGDiceScreen() {
     }
 }
 
-@Preview(showBackground = true)
 @Composable
 fun StatRow(name: String, value: Int, onRoll: () -> Unit) {
     Column {
