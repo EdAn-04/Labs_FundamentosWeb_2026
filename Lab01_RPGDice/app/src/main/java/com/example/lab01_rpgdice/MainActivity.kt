5c63eb76f26946812285372d0932c5c35ca2d768
EdAn-04 <andres.culajay@galileo.edu>
2026-02-01 13:47:34 -0600

    primer commit - creacion de lmainactivity y setcontent para iniciar la app

--- Lab01_RPGDice/app/src/main/java/com/example/lab01_rpgdice/MainActivity.kt
+++ Lab01_RPGDice/app/src/main/java/com/example/lab01_rpgdice/MainActivity.kt
@@ -16,15 +16,9 @@ import com.example.lab01_rpgdice.ui.theme.Lab01_RPGDiceTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             Lab01_RPGDiceTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                RPGDiceScreen()
             }
         }
     }
